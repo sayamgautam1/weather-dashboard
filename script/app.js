7cd929f2eb7a34a22e6994d49ac3abe2ff042810
Sayam Gautam <sayamgautam1@gmail.com>
2022-04-13 14:10:39 +0930

    add function to store value to local storage everytime a new btn is clicked

--- script/app.js
+++ script/app.js
@@ -3,13 +3,17 @@ let searchList = document.getElementById("searchlist");
 let currentLocation = document.getElementById("currentlocation");
 let currentForecast = document.getElementById("current-forecast");
 let currentConditions = document.getElementById("conditions");
+let displayForecastSection = document.querySelector(".rside");
 let city;
+let searchedCity = [];
+let todayDate = moment().format("L");
 let apiKey = "55dea95f4672af9a4915f9b86aaf1de6"; //get from weather api account
 /// event listener to get the city name on enter
 searchInput.addEventListener("keypress", (e) => {
   if (e.key === "Enter") {
     city = searchInput.value;
     getData(city);
+    saveCitySearched(city);
   }
 });
 
@@ -34,22 +38,41 @@ function getData(location) {
 }
 // display current condtions of the city enetered
 function displayCurrentConditions(data) {
-  currentLocation.innerText = data.name;
+  displayForecastSection.style.display = "block";
+  currentConditions.innerText = "";
+  currentLocation.innerText = data.name + " " + todayDate;
   let listitem1 = document.createElement("li");
   listitem1.appendChild(
-    document.createTextNode("Temperature: " + data.main.temp)
+    document.createTextNode("Temperature: " + data.main.temp + "°F")
   );
   currentConditions.appendChild(listitem1);
   let listitem2 = document.createElement("li");
   listitem2.appendChild(
-    document.createTextNode("Humidity: " + data.main.humidity)
+    document.createTextNode("Humidity: " + data.main.humidity + "%")
   );
   currentConditions.appendChild(listitem2);
   let listitem3 = document.createElement("li");
   listitem3.appendChild(
-    document.createTextNode("Windspeed: " + data.wind.speed)
+    document.createTextNode("Windspeed: " + data.wind.speed + "MPH")
   );
   currentConditions.appendChild(listitem3);
+}
+
+//local storage to save searched city
+function saveCitySearched(cityname) {
+  searchList.textContent = "";
+  if (!searchedCity.includes(cityname)) {
+    searchedCity.push(cityname);
+  }
+  searchedCity.forEach((scity) => {
+    let cityDisplayed = document.createElement("button");
+    cityDisplayed.appendChild(document.createTextNode(scity));
+    let list = document.createElement("li");
+    list.appendChild(cityDisplayed);
+    searchList.appendChild(list);
+  });
+
+  localStorage.setItem("cityname", JSON.stringify(searchedCity));
 }
 /* data needed 
 1. coordinates from key cord
